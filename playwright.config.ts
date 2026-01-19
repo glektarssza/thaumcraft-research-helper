8f06ff4091aa843c4879ffe4afea2bbb42f67912
G'lek Tarssza <glek@glektarssza.com>
2026-01-19 08:59:11 -0700

    Try increasing Playwright timeout.

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     testDir: './e2e',
     timeout: 30 * 1000,
     expect: {
-        timeout: 5000
+        timeout: 10000
     },
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
