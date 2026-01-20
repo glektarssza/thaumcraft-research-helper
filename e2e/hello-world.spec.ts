9e37dc4ceaef364b82df74bfa8ac465b9ea152a3
G'lek Tarssza <glek@glektarssza.com>
2026-01-20 16:08:17 -0700

    Rework main tests.

---
+++ e2e/hello-world.spec.ts
@@ -0,0 +1,33 @@
+import {type Page, test, expect} from '@playwright/test';
+
+const getById = (page: Page, id: string) => {
+    if (!id.startsWith('#')) {
+        return page.locator(`#${id}`);
+    }
+    return page.locator(id);
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const getByClass = (page: Page, id: string) => {
+    if (!id.startsWith('.')) {
+        return page.locator(`.${id}`);
+    }
+    return page.locator(id);
+};
+
+const getByName = (page: Page, name: string) => {
+    if (name.startsWith('.') || name.startsWith('#')) {
+        return page.locator(name.substring(1));
+    }
+    return page.locator(name);
+};
+
+test('visits the app root url', async ({page}) => {
+    await page.goto('/');
+    const elem = getById(page, 'hello-world').and(getByName(page, 'h1'));
+    await expect(elem).toHaveText('Hello world!');
+    await expect(elem).toHaveCSS(
+        'color',
+        /red|rgb\(255, 0, 0\)|rgba\(255, 0, 0, 1(.0?)?\)/
+    );
+});
